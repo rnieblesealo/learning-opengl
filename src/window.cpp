74178d10bd9b17001a38e129f15fc5ee876ed1a7
Rafael Niebles <rnieblesealo@gmail.com>
2025-12-14 20:27:50 -0500

    doc: update milestones

--- src/window.cpp
+++ src/window.cpp
@@ -3,8 +3,8 @@
 
 namespace gle
 {
-constexpr GLint WINDOW_WIDTH  = 800;
-constexpr GLint WINDOW_HEIGHT = 600;
+constexpr GLint WINDOW_WIDTH  = 500;
+constexpr GLint WINDOW_HEIGHT = 500;
 
 Window::Window()
 {
